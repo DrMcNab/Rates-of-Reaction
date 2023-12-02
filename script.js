81422c1fc1a44f661d5d48f99b7e5eac75922921
DrMcNab <152734195+DrMcNab@users.noreply.github.com>
2023-12-02 20:48:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var urls = [
         'https://forms.office.com/Pages/ResponsePage.aspx?id=oyzTzM4Wj0KVQTctawUZKdeWF-cF3h9InCh9rgssk7tURFI5MThUTk5LOFBXSFlMSUpMNUxKMDU2TC4u',
-        'https://forms.office.com/Pages/ResponsePage.aspx?id=oyzTzM4Wj0KVQTctawUZKdeWF-cF3h9InCh9rgssk7tUNDU1QzFXOE1FSTdCVlBBRkdESzI2WlczVi4u,
+        'https://forms.office.com/Pages/ResponsePage.aspx?id=oyzTzM4Wj0KVQTctawUZKdeWF-cF3h9InCh9rgssk7tUNDU1QzFXOE1FSTdCVlBBRkdESzI2WlczVi4u',
         'https://forms.office.com/Pages/ResponsePage.aspx?id=oyzTzM4Wj0KVQTctawUZKdeWF-cF3h9InCh9rgssk7tUQTA3UEpYWlE1MlFQSjc1R0FXRFdYRDZTNS4u',
         'https://forms.office.com/Pages/ResponsePage.aspx?id=oyzTzM4Wj0KVQTctawUZKdeWF-cF3h9InCh9rgssk7tUNVBNM1QzOEZSWVJBRjJKREs4OFU4UUJNNC4u'
     ];
